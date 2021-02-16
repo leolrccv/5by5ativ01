fe4f04e8388d79cd912c561c81c0b9c2e4c20a66
leleo <leolrccv@gmail.com>
2021-02-16 17:56:08 -0300

    Exercicio 1

---
+++ Ex01/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Exercicio01 {
+    class Pessoa : Veiculo {
+        public Veiculo veiculo;
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string DataDeNascimento { get; set; }
+        public string DataCompra { get; set; }
+        public override string ToString() {
+            return ($"Nome: {Nome} Cpf: {Cpf} Endereço: {Endereco} Data de Nascimento: {DataDeNascimento}" +
+                    $"\nData de compra: {DataCompra} Marca: {veiculo.Marca} Modelo: {veiculo.Modelo} Cor: {veiculo.Cor}" +
+                    $"\nAno: {veiculo.Ano} Placa: {veiculo.Placa} Renavam: {veiculo.Renavam} Chassi: {veiculo.Chassi}");
+        }
+    }
+}
---
+++ Ex01/Veiculo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Exercicio01 {
+    class Veiculo {
+        public long Renavam { get; set;}
+        public string Chassi { get; set; }
+        public string Placa { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Cor { get; set; }
+        public int Ano { get; set; }
+
+    }
+}
